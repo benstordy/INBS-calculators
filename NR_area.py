9bd6c84f3b3522a99d4edc869ec0045ff6f68477
Ben <ben.stordy@mail.utoronto.ca>
2018-10-06 16:31:33 -0400

    Created a nanorod area calculator (NR_area.py), and added another function to nano_area_calcs.py for nanorod calculations. Modified some comments and output text.

--- NR_area.py
+++ NR_area.py
@@ -1,5 +1,6 @@
 # author: Ben Stordy
-# python based NP concentration calculator
+# python based NR concentration calculator
+# assumes a hemispherical-capped cylinder rod
 # takes command line inputs
 
 # imports the sys module, and the argparse module, allowing for command line
@@ -15,9 +16,10 @@ print("running", sys.argv[0], "\n")
 # define parse to be the funtion ArgumentParser from the argparse module
 parse = argparse.ArgumentParser()
 # use the add_argument function to specify which command line options the
-# calculator will accept. We need three inputs, the diameter, concentration,
+# calculator will accept. We need four inputs, the length, width, concentration,
 # and target area.
-parse.add_argument("diameter", type=float, help='the diameter of the NP (in nm)')
+parse.add_argument("length", type=float, help='the length of the NR (in nm)')
+parse.add_argument("width", type=float, help='the width of the NR (in nm)')
 parse.add_argument("concentration", type=float, help="the concentration of the"
                     "NP solution (in nM)")
 parse.add_argument("target_area", type=float, help="the target area of the NP"
@@ -28,22 +30,20 @@ parse.add_argument("-v", "--verbose", action="store_true", help="increase "
 args = parse.parse_args()
 
 # use the nano_area_calcs formulae to calculate the area, number, mols, and
-# volume of the nanoparticles
-area = calcs.area_NP(args.diameter)
+# volume of the nanorods
+area = calcs.area_NR(args.length, args.width)
 number = calcs.number_NP(area, args.target_area)
 mols = calcs.mols_NP(number)
 volume = calcs.volume_NP(args.concentration, mols)
 
-# decide how much of this info to output, depending whether the user wants
-# verbosity
 if args.verbose:
-    print("The area of a single nanoparticle is", "%.4E" % area, "cm^2 \n")
-    print("The number of nanoparticles required for", args.target_area, "cm^2",
+    print("The area of a single nanorod is", "%.4E" % area, "cm^2 \n")
+    print("The number of nanorods required for", args.target_area, "cm^2",
         "is", "%.4E" % number, "\n")
     print("The number of moles required for", args.target_area, "cm^2 is",
         "%.4E" % mols,"\n")
-    print("The volume of", args.concentration, "nM nanoparticle solution",
-        "required for", args.target_area, "cm^2 is", "%.2f" % volume, "uL \n")
+    print("The volume of", args.concentration, "nM nanorod solution required",
+        "for", args.target_area, "cm^2 is", "%.2f" % volume, "uL \n")
 else:
     print("Use", "%.2f" % volume, "uL for", args.target_area, "cm^2 \n \nFor",
         "more detail, try including the optional flag '-v' in your input \n")
--- nano_area_calcs.py
+++ nano_area_calcs.py
@@ -6,7 +6,8 @@ from scipy import constants
 
 # create a function to calculate the area of a sphere in square centimetres,
 # taking diameter in nanometres as an argument.
-def sphere_area(diameter):
+def area_NP(diameter):
+    # get the surface area of a sphere from its diameter.
     area = constants.pi*diameter**2*1e-14
     return area
 
@@ -14,11 +15,14 @@ def sphere_area(diameter):
 # required to acheive the target area. Both arguments are to be given in square
 # centimetres.
 def number_NP(area, target_area):
+    # get the number of nanoparticles required by taking the ratio of the target
+    # area to the area per nanoparticle.
     num = target_area/area
     return num
 
 # create a function to convert to the moles of nanoparticles from a number.
 def mols_NP(num):
+    # use avogadro's constant to get moles from a number.
     mol = num/constants.Avogadro
     return mol
 
@@ -26,5 +30,19 @@ def mols_NP(num):
 # to acheive the target area. Concentration is to be given in nanomolar, while
 # mol (required moles) is given in moles.
 def volume_NP(concentration, mol):
+    # take the ratio of the desired number of moles over the solution
+    # concentration to get solution volume required
     vol = (mol/(concentration*1e-9))*1e6
     return vol
+
+# create a function to calculate the area of a rod in square centimetres,
+# taking length and width in nanometres as arguments.
+def area_NR(length, width):
+    # calculate area of a sphere with diameter = width of NR (in cm^2).
+    caps_area = constants.pi*width**2*1e-14
+    # calculate area of the side of a cylinder with cylinder length =
+    # length of NR - width of NR, and diameter = width of NR (in cm^2).
+    side_area = constants.pi*width*(length-width)*1e-14
+    # sum to get total area.
+    area = side_area+caps_area
+    return area
