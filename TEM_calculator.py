096707c50a07930b7f6b97f3c40f1312dd6874a1
Ben <ben.stordy@mail.utoronto.ca>
2018-10-31 16:53:00 -0400

    working version of the TEM_calculator file, as well as sample input and output files.

--- TEM_calculator.py
+++ TEM_calculator.py
@@ -17,20 +17,21 @@ parse = argparse.ArgumentParser()
 
 # use the add_argument function to specify which command line options the
 # calculator will accept.
-parse.add_argument("filename", type=str, help="the filename of your excel "
+parse.add_argument("filename", type=str, help="The filename of your excel "
                     "sheet. This cannot contain spaces.")
 parse.add_argument("sheetname", type=str, nargs="?", default="Sheet1", help=
-                    "The name of the sheet in the excel file that contains the "
-                    "data to be analysed. By default this is 'Sheet1'.")
-parse.add_argument("-p", "--plot", action="store_true", help="plot histogram of"
+                    "(Optional) The name of the sheet in the excel file that "
+                    "contains the data to be analysed. By default this is "
+                    "'Sheet1'.")
+parse.add_argument("output", type=str, nargs="?", default="output.xlsx", help=
+                    "(Optional) The desired name of the output excel file. By "
+                    "default this is 'ouput.xlsx'.")
+parse.add_argument("-p", "--plot", action="store_true", help="Plot histogram of"
                     " the data.")
+parse.add_argument("-s", "--save", action="store_true", help="Save data to an "
+                    " excel file.")
 # parse the arguments passed in the command line
 args = parse.parse_args()
-
-# check that sensible input values have been given
-# if(args.filename ):
-#     sys.exit("Error: You must provide an excel file as your input! \nUse the"
-#         " help flag '-h' if you need help using the calculator.\n")
 
 # get the path to the file (will vary for different users)
 # path =("~/Documents/Academic/OneDrive/OneDrive\ -\ University\ of\ Toronto/"
@@ -39,6 +40,7 @@ args = parse.parse_args()
 # load the spreadsheet
 xl = pd.ExcelFile(args.filename)
 TEM_df = xl.parse(args.sheetname)
+writer = pd.ExcelWriter(args.output)
 
 # get the conversion factor from pixels to nanometres
 factors = TEM_df.scale/TEM_df.length
@@ -50,27 +52,40 @@ TEM_df['factor'] = factors.where(TEM_df.img.notnull())
 TEM_df['factor'] = TEM_df['factor'].fillna(method='ffill')
 # use the scale factor to convert from a pixel length to the size in nanometres
 TEM_df['size'] = TEM_df.length*TEM_df.factor
+
 # get the rows of the 'size' column that do not contain scalebar measurements
-TEM_df = TEM_df['size'][TEM_df['scale'].isnull()]
+size_df = TEM_df['size'][TEM_df['scale'].isnull()]
 # separate the odd and even elements of the size
-TEM_df = pd.DataFrame({ 'nm_length' : TEM_df.iloc[::2], 'nm_width' :
-                            TEM_df.iloc[1::2]})
+size_df = pd.DataFrame({ 'nm_length' : size_df.iloc[::2], 'nm_width' :
+                            size_df.iloc[1::2]})
+
 # create a new dataframe to hold the lengths of the nanorods
-nm_measured_df = TEM_df.dropna(subset = ['nm_length']).reset_index(drop=True)
+nm_measured_df = size_df.dropna(subset = ['nm_length']).reset_index(drop=True)
 # create a new dataframe to hold the widths of the nanorods
-nm_width = TEM_df.dropna(subset = ['nm_width']).reset_index(drop=True)
+nm_width = size_df.dropna(subset = ['nm_width']).reset_index(drop=True)
 # combine the widths and lengths side-by-side for each measured nanorod
 nm_measured_df['nm_width'] = nm_width['nm_width']
 # calculate the aspect ratio of each nanorod
 nm_measured_df['nm_AR'] = nm_measured_df.nm_length/nm_measured_df.nm_width
 
+# apply some statistics
 means = nm_measured_df.mean()
 stdDev = nm_measured_df.std()
-results = pd.DataFrame({'parameter':means.index, 'mean':means.values, 'std dev':
-                        stdDev.values})
 n = nm_measured_df.count()
-print(results, '\nnumber of observations: ', n[1])
 
+# create a results dataframe
+results = pd.DataFrame({'parameter':means.index, 'mean':means.values, 'std dev':
+                        stdDev.values, 'N':n.values})
+# output the results
+print(results)
+
+# save the data if desired
+if args.save:
+    TEM_df.to_excel(writer, sheet_name='data')
+    results.to_excel(writer, sheet_name='results')
+    writer.save()
+
+# plot a histogram if desired
 if args.plot:
     nm_measured_df.hist()
     plt.show()
